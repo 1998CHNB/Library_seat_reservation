16bc9cf272159eddb29e141bde137b2582096b2c
chenhao <48203581+1998CHNB@users.noreply.github.com>
2020-01-06 08:36:36 +0800

    Update Library_Reservation.py

--- Library_Reservation.py
+++ Library_Reservation.py
@@ -18,19 +18,19 @@ def get():
     yuyuehour = int(yuyuetime[0])*10+int(yuyuetime[1])
     yuyueminute = int(yuyuetime[3])*10+int(yuyuetime[4])
     dt=datetime.now()
-    sleeptime = int(yuyueday)*24*3600+(yuyuehour-dt.hour)*3600+(yuyueminute-dt.minute)*60
+    sleeptime = int(yuyueday)*24*3600+(yuyuehour-dt.hour)*3600+(yuyueminute-dt.minute)*60-dt.second
     t = Timer(sleeptime, reservation)
     t.start()
 window = tk.Tk()
 window.title('图书馆座位预约')
 window.geometry('1000x700')
-l = tk.Label(window, text='你好，为保证预约成功，请提前登陆微信，并把公众号：“我去图书馆” 置顶。', bg='blue', font=('Arial', 12), width=60, height=2)
+l = tk.Label(window, text='你好，为保证预约成功，请提前登陆微信，并最好置顶公众号：“我去图书馆” 。', bg='blue', font=('Arial', 12), width=60, height=2)
 l.pack()
 tishi1 = tk.Label(window, text='请先输入你想要几天后预约:', font=('Arial', 12), width=30, height=1)
 tishi1.pack()
 e1 = tk.Entry(window, show = None)
 e1.pack()
-tishi2 = tk.Label(window, text='请输入你想要预约的具体时间:(格式-例如：6:01请输入：06:01)', font=('Arial', 12), width=50, height=1)
+tishi2 = tk.Label(window, text='请输入你想要预约当天的具体时间:(格式-例如：6:01请输入：06:01)', font=('Arial', 12), width=55, height=1)
 tishi2.pack()
 e2 = tk.Entry(window, show = None)
 e2.pack()
@@ -38,9 +38,9 @@ b1 = tk.Button(window, text='输入确定', font=('Arial', 12),width=10,height=1
 b1.pack()
 l1 = tk.Label(window, text='', font=('Arial', 12), width=1, height=14)
 l1.pack()
-l2 = tk.Label(window, text='初次使用时请将文件夹下的png图片换成自己电脑上对应的截图',font=('Arial', 12), width=50, height=1)
+l2 = tk.Label(window, text='初次使用时最好将文件夹下的png图片换成自己电脑上对应的截图',font=('Arial', 12), width=50, height=1)
 l2.pack()
-l3 = tk.Label(window, text='这样能使图像识别的准确率提升',font=('Arial', 12), width=40, height=1)
+l3 = tk.Label(window, text='这样能使图像识别的准确率提升；图片和exe文件需放在同一个文件夹下',font=('Arial', 12), width=55, height=1)
 l3.pack()
 def reservation():
     T = 0
@@ -80,36 +80,48 @@ def reservation():
             time.sleep(1)
         hwnd2 = win32gui.FindWindow(None,AppName)
         win32gui.SetWindowPos(hwnd2,win32con.HWND_TOPMOST,0,0,int(pingmu_X/3),int(pingmu_Y/1.1),win32con.SWP_SHOWWINDOW)
-        time.sleep(3)
-        try:
-            cposition = pyautogui.locateOnScreen('qiandao.png')
-            cc = pyautogui.center(cposition)[0]
-            win32gui.PostMessage(hwnd1, win32con.WM_CLOSE, 0, 0)
-            win32gui.PostMessage(hwnd2, win32con.WM_CLOSE, 0, 0)
-        except:
-            pass
+        time.sleep(2)
         try:
             cposition = pyautogui.locateOnScreen('zuoweibiaozhi.png')  
             cc = pyautogui.center(cposition)
-            pyautogui.moveTo(cc[0]-int(pingmu_X/4.5),cc[1])  
+            pyautogui.moveTo(cc[0]-int(pingmu_X/4.5),cc[1])
             pyautogui.click(clicks=1)
             time.sleep(3)
             pyautogui.moveTo(cc[0]-int(pingmu_X/11),cc[1])  
             pyautogui.click(clicks=1)
             time.sleep(3)
         except:
+            time.sleep(4)
             try:
-                cposition = pyautogui.locateOnScreen('changyongzuowei.png')  
+                cposition = pyautogui.locateOnScreen('zuoweibiaozhi.png')  
                 cc = pyautogui.center(cposition)
-                pyautogui.moveTo(cc[0]-int(pingmu_X/11),cc[1]+int(pingmu_Y/1.9))  
+                pyautogui.moveTo(cc[0]-int(pingmu_X/4.5),cc[1])
                 pyautogui.click(clicks=1)
                 time.sleep(3)
-                pyautogui.moveTo(cc[0]+int(pingmu_X/11),cc[1]+int(pingmu_Y/1.9))  
+                pyautogui.moveTo(cc[0]-int(pingmu_X/11),cc[1])  
                 pyautogui.click(clicks=1)
                 time.sleep(3)
             except:
-                move_click(int(pingmu_X/11),int(pingmu_Y/1.8),3)
-                move_click(int(pingmu_X/4.9),int(pingmu_Y/1.8),3)
+                try:
+                    cposition = pyautogui.locateOnScreen('changyongzuowei.png')  
+                    cc = pyautogui.center(cposition)
+                    pyautogui.moveTo(cc[0]-int(pingmu_X/11),cc[1]+int(pingmu_Y/1.9))  
+                    pyautogui.click(clicks=1)
+                    time.sleep(3)
+                    pyautogui.moveTo(cc[0]+int(pingmu_X/13),cc[1]+int(pingmu_Y/1.9))  
+                    pyautogui.click(clicks=1)
+                    time.sleep(3)
+                except:
+                    move_click(int(pingmu_X/11),int(pingmu_Y/1.7),3)
+                    move_click(int(pingmu_X/4.6),int(pingmu_Y/1.7),3)
+        try:
+            cposition = pyautogui.locateOnScreen('qiandao.png')
+            cc = pyautogui.center(cposition)[0]
+            win32gui.PostMessage(hwnd1, win32con.WM_CLOSE, 0, 0)
+            win32gui.PostMessage(hwnd2, win32con.WM_CLOSE, 0, 0)
+            T+=2
+        except:
+            pass
         win32gui.PostMessage(hwnd1, win32con.WM_CLOSE, 0, 0)
         win32gui.PostMessage(hwnd2, win32con.WM_CLOSE, 0, 0)
         T+=1
